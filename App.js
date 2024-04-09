94ef7ae32d7a2431d2d207193908b0ce3c832838
WilsonArriola <warriol@gmail.com>
2024-04-09 05:10:07 -0300

    init

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [error, setError] = useState('');
+
+  const [credentials, setCredentials] = useState({
+    username: "willy",
+    password: "1234",
+    google: ""
+  });
+
+  async function login() {
+
+    fetch('http://192.168.4.178:8080/login', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify(credentials)
+    })
+    .then(res => {
+      if (res.status !== 200) {
+        if (res.status === 401) 
+          setError('Credenciales incorrectas.');
+        else
+          setError('Error en la conexión.');
+      }
+      return res.json();
+    })
+    .then(data => {
+      setError(data.token);
+      setCredentials({ ...credentials, google: data.message });
+      //setCredentials({ google: data });
+      return data;
+    })
+    .catch(error => {
+      setError('Error en la conexión: ' + error);
+    });
+
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola willy!</Text>
+      <Text onPress={login}>Login</Text>
+      <Text>{error}</Text>
+      <Text>{credentials.google}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ readme.md
@@ -0,0 +1,14 @@
+# instalar cli
+npm i -g expo-cli
+
+# crear proyecto en blanco
+npx  create-expo.app proy24mobile-1
+
+# instalr dependencias
+npx expo install
+
+# iniciar app
+npx expo start
+
+# para que funcion la api, hay que abrir en el celular con la url
+# ademas la direccón de la api debe apuntar a la ip (no a localost)
